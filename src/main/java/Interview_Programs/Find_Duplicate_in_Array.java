f34bb5662b562f9326d5fc16f3fe5ea4cb4ddc23
Abhishek Tyagi <abhishektyagi99831@gmail.com>
2025-07-08 12:11:22 +0530

    remove dulicate elements in array

--- src/main/java/Interview_Programs/Find_Duplicate_in_Array.java
+++ src/main/java/Interview_Programs/Find_Duplicate_in_Array.java
@@ -6,16 +6,32 @@ public class Find_Duplicate_in_Array {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr = {1,2,3,3,2,5,6,7,8,9,1};
-		
+		int[] arr = { 1, 2, 3, 3, 2, 5, 6, 7, 8, 9, 1 };
+
 		HashSet<Integer> value = new HashSet<>();
 		HashSet<Integer> duplicate = new HashSet<>();
-		
+
+		// Duplicate Elements in Array
 		for (int num : arr) {
 			if (!value.add(num)) {
 				duplicate.add(num);
 			}
 		}
+
+		/*
+		 * // Remove Duplicate elements in Array 
+		 * //HashSet<Integer> remove = new HashSet<>(); 
+		 * for (int num : arr)
+		 *  {
+			 *  if (value.add(num)) 
+			 *  { 
+			 *  	remove.add(num); 
+			 *  }
+		 *   }
+		 * 
+		 * System.out.println("Remove Duplicate value is : " + remove);
+		 */
+		
 		System.out.println("Duplicate value is : " + duplicate);
 	}
 
